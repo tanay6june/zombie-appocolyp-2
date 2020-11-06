7d28f0dac40b94bd50ff9777b02f5f27384ca80a
tanay6june <72078342+tanay6june@users.noreply.github.com>
2020-11-06 12:32:56 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,95 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground;
+var box1,box2,box3;
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	//var helicopterSprite_options ={
+	//	isStatic : false
+	//}
+	//var packageSprite_options ={
+	//	restitution = 3
+	//}
+
+	box1=new box1(275,745,200,20);
+	box1.shapeColor= "red"
+	box2=new box1(275,745,20,100);
+	box2.shapeColor= "red"
+	box3=new box1(375,745,20,100);
+	box3.shapeColor= "red"
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:3, isStatic:true});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+	box1 = Bodies.rectangle(width/2 , 200 , 5 , {isStatic:true});
+	 World.add(world,box1);
+	 box2 = Bodies.rectangle(width/2 , 200 , 5 , {isStatic:true});
+	 World.add(world,box2);
+	 box3 = Bodies.rectangle(width/2 , 200 , 5 , {isStatic:true});
+	 World.add(world,box3);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  Engine.update(engine);
+
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+
+  box1.display();
+  box2.display();
+  box3.display();
+
+  keyPressed();
+ 
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+	Matter.Body.setStatic(packageBody,false)
+    
+ }
+}
+
+
+
